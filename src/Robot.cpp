bb59c9d41fec57fd0ff0b369b773672bbb65f344
3197Software <3197Software@gmail.com>
2018-05-08 19:23:57 -0500

    change drive to pointer and update references

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -4,13 +4,13 @@ class Robot: public frc::IterativeRobot {
 public:
 	Spark* left = new Spark(0);
 	Spark* right = new Spark(1);
-	DifferentialDrive drive = new DifferentialDrive(left, right);
+	DifferentialDrive* drive = new DifferentialDrive(*left, *right);
 	Joystick* j = new Joystick(0);
 
 	void TeleopInit() {
 	}
 	void TeleopPeriodic() {
-		drive.ArcadeDrive(j->GetRawAxis(1), j->GetRawAxis(5));
+		drive->ArcadeDrive(j->GetRawAxis(1), j->GetRawAxis(5));
 	}
 };
 
