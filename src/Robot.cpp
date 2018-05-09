de78f715473e35d5e98637c1bcee9bc895b0917e
3197Software <3197Software@gmail.com>
2018-05-08 20:52:42 -0500

    added turret position

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -2,15 +2,23 @@
 
 class Robot: public frc::IterativeRobot {
 public:
-	Spark* left = new Spark(0);
-	Spark* right = new Spark(1);
+	Spark* left = new Spark(1);
+	Spark* right = new Spark(2);
+
+	Spark* turretPitch = new Spark(3);
+	Spark* turretYaw = new Spark(4);
+
 	DifferentialDrive* drive = new DifferentialDrive(*left, *right);
 	Joystick* j = new Joystick(0);
 
-	void TeleopInit() {
+	void TeleopInit() override {
 	}
-	void TeleopPeriodic() {
-		drive->ArcadeDrive(j->GetRawAxis(1), j->GetRawAxis(5));
+	void TeleopPeriodic() override{
+		drive->TankDrive(j->GetRawAxis(5), j->GetRawAxis(1));
+		turretPitch->Set((j->GetRawButton(6) ? 1 : (j->GetRawButton(5)) ? -1 : 0)); //rb right lb left
+		turretYaw->Set(j->GetRawAxis(3) - j->GetRawAxis(2)); //rb right lb left
+
+
 	}
 };
 
