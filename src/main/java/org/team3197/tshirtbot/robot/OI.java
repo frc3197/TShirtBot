4613f637e4a5b9cd97a8bb3ab31c7cd94b99d999
Spencer <spencers0730@gmail.com>
2019-03-05 22:11:08 -0600

    port to java

---
+++ src/main/java/org/team3197/tshirtbot/robot/OI.java
@@ -0,0 +1,28 @@
+package org.team3197.tshirtbot.robot;
+
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.XboxController;
+
+public final class OI {
+    private static XboxController joystick = new XboxController(0);
+
+    public static final double xSpeed() {
+        return joystick.getY(Hand.kRight);
+    }
+
+    public static final double rSpeed() {
+        return joystick.getX(Hand.kRight);
+    }
+
+    public static final double pitchSpeed() {
+        return joystick.getY(Hand.kLeft);
+    }
+
+    public static final double yawSpeed() {
+        return joystick.getX(Hand.kLeft);
+    }
+
+    public static final boolean getShoot() {
+        return joystick.getAButton();
+    }
+}
